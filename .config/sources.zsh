c15823510455784ff18472b02376acb58e9406b8
Daniel W <github@danieljw.net>
2020-05-04 10:40:16 +1000

    Create sources.zsh

---
+++ .config/sources.zsh
@@ -0,0 +1,7 @@
+# Heroku CLI (autocomplete)
+HEROKU_AC_ZSH_SETUP_PATH=$HOME/Library/Caches/heroku/autocomplete/zsh_setup
+[ -f $HEROKU_AC_ZSH_SETUP_PATH ] && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
+
+# Travis CLI
+TRAVISCLI_SETUP_PATH=$HOME/.travis/travis.sh
+[ -f $TRAVISCLI_SETUP_PATH ] && source $TRAVISCLI_SETUP_PATH
