24df02d916cc5dadd3d918b9d773e95d4be7bbaa
Daniel W <github@danieljw.net>
2020-05-04 10:23:18 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-if [ ! -d "${HOME}/.dotfiles" ] then;
+set -euo pipefail
+
+if [ ! -d "${HOME}/.dotfiles" ] then
   git clone git@github.com:djw4/dotfiles.git $HOME/.dotfiles
 fi
 
-if [ ! -d "${HOME}/.config" ] then;
+if [ ! -d "${HOME}/.config" ] then
   mkdir -v $HOME/.config
 fi
 
